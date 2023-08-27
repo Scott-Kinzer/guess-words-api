a6cb661a631b753acf0a20eefa9d5be2be8e37b6
Scott-Kinzer <zaz765831@gmail.com>
2023-08-27 09:58:14 +0300

    feat: implemented refresh token endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { PincodeUserDto, RegisterUserDto } from './types/auth.dto';
+import {
+  PincodeUserDto,
+  RefreshTokensDto,
+  RegisterUserDto,
+} from './types/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hashPassword } from 'src/helpers/hashPassword';
 import { generatePincode } from 'src/helpers/generatePincode';
@@ -156,5 +160,55 @@ export class AuthService {
     const refreshToken = this.jwtService.sign(user, { expiresIn: '7d' });
 
     return { accessToken, refreshToken };
+  }
+
+  async refreshToken(refreshTokens: RefreshTokensDto) {
+    try {
+      this.jwtService.verify(refreshTokens.refreshToken);
+
+      const tokenData = this.jwtService.decode(refreshTokens.refreshToken) as {
+        id: string;
+        email: string;
+      };
+
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          id: tokenData.id,
+          email: tokenData.email,
+        },
+      });
+
+      if (!user) {
+        throw new BadRequestException('Not valid tokens');
+      }
+
+      const isTokensExist = await this.prismaService.authToken.findUnique({
+        where: {
+          userId: user.id,
+          accessToken: refreshTokens.accessToken,
+          refreshToken: refreshTokens.refreshToken,
+        },
+      });
+
+      if (!isTokensExist) {
+        throw new BadRequestException('Not valid tokens');
+      }
+
+      const tokens = this.generateTokens({ id: user.id, email: user.email });
+
+      await this.prismaService.authToken.update({
+        where: {
+          userId: user.id,
+        },
+        data: {
+          accessToken: tokens.accessToken,
+          refreshToken: tokens.refreshToken,
+        },
+      });
+
+      return tokens;
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
   }
 }
